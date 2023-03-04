41c908d40018a00e8c76c66b108443008bc29610
Nitesh-mint <nitesh.raya058@gmail.com>
2023-03-04 21:25:02 +0545

    initial commit

--- students/urls.py
+++ students/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name='index'),
+    path("<int:id>", views.student_view, name='student_view'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Student
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 def index(request):
     return render(request, 'students/index.html', {
         'students': Student.objects.all() #students bhanne ma Student class ko all of the table cha
     })
+
+def student_view(request , id):
+    student = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
